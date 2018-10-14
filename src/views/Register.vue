5157244890661266cebdc16aa2a9064e106a7b7a
Sergey Luschik <frostbolt@live.ru>
2018-10-14 14:42:48 +0300

    start templating registration page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,6 +4,27 @@
 		<p v-if="!getInfo.isRegistraionActive">
 			Регистрация временно недоступна.
 		</p>
+		<div v-if="getInfo.isRegistraionActive">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae repudiandae vero nesciunt excepturi laudantium unde fuga eum alias magni?</p>
+
+			<h3>Командная олимпиада</h3>
+			<p>Velit consequatur quo tempore, necessitatibus ullam minima eius rerum dolore.</p>
+			<div class="reg-link">				
+				<a href="#">Перейти к регистрации команды</a>
+			</div>
+
+			<h3>Мини-курсы и мастер-классы</h3>
+			<p>Velit consequatur quo tempore, necessitatibus ullam minima eius rerum dolore.</p>
+			<div class="reg-link">				
+				<a href="#">Перейти к регистрации участников мини-курсов и мастер-классов</a>
+			</div>
+
+			<h3>Конкурс компьютерных работ</h3>
+			<p>Velit consequatur quo tempore, necessitatibus ullam minima eius rerum dolore.</p>
+			<div class="reg-link">				
+				<a href="#">Перейти к регистрации конкурсной работы</a>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -17,3 +38,14 @@
 		}
 	}
 </script>
+
+<style lang="scss" scoped>
+	.reg-link {
+		text-align: right;
+	}
+
+	h3 {
+		margin-top: 30px;
+	}
+
+</style>
