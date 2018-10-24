ec9f0cccf33d452acf9fe08fe8bf8f676d013430
Sergey Luschik <Sergey.Lushchik@vmedia.ca>
2018-10-24 15:49:41 +0300

    fix routing behavior between pages. remove legacy files (part 2)

--- src/views/Page.vue
+++ src/views/Page.vue
@@ -4,43 +4,43 @@
 </template>
 
 <script lang="ts">
-	import { Component, Prop, Vue } from 'vue-property-decorator';
-	import axios from 'axios';
-	import { apiHost } from '@/config';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+import { apiHost } from '@/config';
 
-	Component.registerHooks([
-		'beforeRouteUpdate',
-	]);
+Component.registerHooks([
+	'beforeRouteUpdate',
+]);
 
-	@Component
-	export default class Page extends Vue {
-		public pageContent = '';
-		public pageTitle = '';
-		public pageSlug = '';
+@Component
+export default class Page extends Vue {
+	public pageContent = '';
+	public pageTitle = '';
+	public pageSlug = '';
 
-		constructor() {
-			super();
-		}
-
-		public beforeRouteUpdate(to: any, from: any, next: () => {}) {
-			this.fillInWithContent(this.$route.params.id);
-			next();
-		}
-
-		public mounted() {
-			this.fillInWithContent(this.$route.params.id);
-		}
-
-		private fillInWithContent(id: string) {
-			axios
-				.get(`${apiHost}/pages/${id}`)
-				.then((resp: any) => {
-					this.pageContent = resp.data.content;
-					this.pageTitle = resp.data.title;
-					this.pageSlug = resp.data.slug;
-				}, () => {
-					this.$router.replace('/404');
-				});	
-		}
+	constructor() {
+		super();
 	}
+
+	public beforeRouteUpdate(to: any, from: any, next: () => {}) {
+		this.fillInWithContent(to.params.id);
+		next();
+	}
+
+	public mounted() {
+		this.fillInWithContent(this.$route.params.id);
+	}
+
+	private fillInWithContent(id: string) {
+		axios
+			.get(`${apiHost}/pages/${id}`)
+			.then((resp: any) => {
+				this.pageContent = resp.data.content;
+				this.pageTitle = resp.data.title;
+				this.pageSlug = resp.data.slug;
+			}, () => {
+				this.$router.replace('/404');
+			});
+	}
+}
 </script>
