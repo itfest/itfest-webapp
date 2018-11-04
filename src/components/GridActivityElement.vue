cf82c6052895ffc84a0cc159c856094ff3b99bcc
Sergey Luschik <frostbolt@live.ru>
2018-11-04 03:39:27 +0300

    :new_moon_with_face:

--- src/components/GridActivityElement.vue
+++ src/components/GridActivityElement.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="activities__item"
-		:class="{'activities__item--no-text': isWithoutText()}"
+		:class="{'activities__item--no-text': (activityItem.text.length > 0 && activityItem.linkUri.length > 0)}"
 	>
 		<span>
 			<h2 >{{ activityItem.heading }}</h2>
-			<p v-if=!isWithoutText()>{{ activityItem.text }}</p>
-			<a v-if=!isWithoutText() :href=activityItem.linkUri>
+			<p v-if="activityItem.text.length > 0">{{ activityItem.text }}</p>
+			<a v-if="activityItem.linkUri.length > 0" :href=activityItem.linkUri>
 				{{ activityItem.linkText }}
 			</a>
 		</span>
@@ -19,9 +19,5 @@
 	@Component
 	export default class GridActivityElement extends Vue {
 		@Prop() private activityItem!: GridListObject;
-
-		public isWithoutText() {
-			return this.activityItem.linkUri.length === 0;
-		}
 	}
 </script>
